351226eba51df58112b10cf43433dfbd7a1126c5
jacky-ui <jacky.du.004@gmail.com>
2022-06-17 12:01:22 -0400

    if token is false, then page will render link to go to login page

--- script/index.js
+++ script/index.js
@@ -1,15 +1,24 @@
-console.log(sessionStorage.token);
+// Grab section tag from html body
 const sectionElement = document.querySelector(".main");
 
+// Declare and assign token from sessionStorage to variable
 let token = sessionStorage.token;
 
+// Might make function here to create and append to sectionElement
+
+// Depending on status of token, different things will render on page
 if (token === undefined) {
     console.log("Please present proof");
 
     const header = document.createElement("h1");
     header.innerText = "Please login to view page";
+
+    const loginLink = document.createElement("a");
+    loginLink.setAttribute("href", "./pages/login.html");
+    loginLink.innerText = "Click here to login!";
+
     sectionElement.appendChild(header);
-    console.log(sectionElement);
+    sectionElement.appendChild(loginLink);
 
 }   else {
     console.log("Continue Please");

1f80d03d8a8a9638c110633fa44c99b4d87b4152
jacky-ui <jacky.du.004@gmail.com>
2022-06-17 11:37:37 -0400

    create basic if statment that will check if sessionStorage contains token.

--- script/index.js
+++ script/index.js
@@ -1,1 +1,12 @@
 console.log(sessionStorage);
+
+console.log(sessionStorage.token);
+
+let token = sessionStorage.token;
+token = "fdsfdsfasdf";
+
+if (token === undefined) {
+    console.log("Please present proof");
+}   else {
+    console.log("Continue Please");
+}

ad918e56ead71d2c84ad75c5eab8a961b8ad1726
jacky-ui <jacky.du.004@gmail.com>
2022-06-17 15:46:14 -0400

    able to target form values and assign to variables. Also added in preventDefault for testing for now

--- script/login.js
+++ script/login.js
@@ -1,2 +1,9 @@
 const userInput = document.querySelector("form");
-console.log(userInput);
+
+userInput.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const username = event.target.username.value;
+    const password = event.target.username.value;
+    console.log(username, password);
+})

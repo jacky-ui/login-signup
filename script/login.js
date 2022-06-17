8e02df10dc46e14a2446d5f9e1c92f783372adb5
jacky-ui <jacky.du.004@gmail.com>
2022-06-17 15:48:24 -0400

    create basic function that will evaluate if form fields are empty.

--- script/login.js
+++ script/login.js
@@ -4,6 +4,12 @@ userInput.addEventListener("submit", (event) => {
     event.preventDefault();
 
     const username = event.target.username.value;
-    const password = event.target.username.value;
+    const password = event.target.password.value;
     console.log(username, password);
+
+    if (!username || !password) {
+        console.log("Please fill in all fields");
+    }   else {
+        console.log("Success");
+    }
 })

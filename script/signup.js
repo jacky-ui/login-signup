864ee08586372b3e86cfd45e04997f5eb40bafee
jacky-ui <jacky.du.004@gmail.com>
2022-06-27 11:15:05 -0400

    create and test function that will console.log user inputs

--- script/signup.js
+++ script/signup.js
@@ -1,1 +1,8 @@
-console.log("Hello world");
+const userForm = document.querySelector("form");
+console.log(userForm);
+
+userForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    console.log(event.target.username.value);
+    console.log(event.target.password.value);
+})

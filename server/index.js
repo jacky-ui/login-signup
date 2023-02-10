fbb7bb412b0d35c94c5e62f1067a1799336740e9
jacky-ui <jacky.du.004@gmail.com>
2023-02-10 08:13:16 -0500

    testing again

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 require("dotenv").config();
 // const { PORT } = process.env;
 const usersRoutes = require("./routes/usersRoutes");
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 // Using cors and json files
 app.use(cors());
 app.use(express.json());
@@ -14,6 +14,6 @@ app.use(express.static("assets"));
 // Routes for different requests
 app.use("/", usersRoutes);
 
-app.listen(process.env.PORT || 5000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("Server is running on PORT 5000");
 });

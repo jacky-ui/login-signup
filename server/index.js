75c4180397e10b62c8a6435aeb66e2252a2541f4
jacky-ui <jacky.du.004@gmail.com>
2023-02-10 08:04:16 -0500

    changes

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 require("dotenv").config();
 // const { PORT } = process.env;
 const usersRoutes = require("./routes/usersRoutes");
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 // Using cors and json files
 app.use(cors());
 app.use(express.json());

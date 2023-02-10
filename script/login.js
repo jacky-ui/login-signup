0db9fd76bfc201af393502fdc05413edcc1a7f25
jacky-ui <jacky.du.004@gmail.com>
2023-02-10 08:08:11 -0500

    changed port number on loginjs

--- script/login.js
+++ script/login.js
@@ -20,7 +20,7 @@ userInput.addEventListener("submit", (event) => {
     }   
         else {
             axios
-                .post("https://byzantium-cockatoo-hose.cyclic.app/login", {
+                .post("https://byzantium-cockatoo-hose.cyclic.app:5000/login", {
                     "username": username,
                     "password": password,
                 })

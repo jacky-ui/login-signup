6f38ddfa22b5a7553c4a2f7b0e5bffdb03eb18fd
jacky-ui <jacky.du.004@gmail.com>
2023-02-10 08:26:49 -0500

    change file location in utilsjs

--- server/utils.js
+++ server/utils.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 
 // Function to read parse and read JSON file
 function readUsers() {
-    const users = fs.readFileSync("./assets/users.json");
+    const users = fs.readFileSync("server/assets/users.json");
     const parsedUsers = JSON.parse(users);
     return parsedUsers;
 };

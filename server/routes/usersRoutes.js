fd46c8630ab304a495c135c12389299f1c91fa50
jacky-ui <jacky.du.004@gmail.com>
2023-04-21 13:33:43 -0400

    reverted changes for s3 bucket

--- server/routes/usersRoutes.js
+++ server/routes/usersRoutes.js
@@ -6,8 +6,6 @@ const jwt = require("jsonwebtoken");
 const JWT_KEY = process.env.JWT_KEY;
 const bcrypt = require("bcrypt");
 const uniqid = require("uniqid");
-const AWS = require("aws-sdk");
-const s3 = new AWS.S3();
 
 router.use(express.json());
 router.use(bodyParser.json());
@@ -33,14 +31,9 @@ router.post("/signup", (req, res) => {
             "password": hashedPassword,
         };
 
-        s3.putObject({
-            Body: JSON.stringify({key:newUser}),
-            Bucket: "cyclic-byzantium-cockatoo-hose-ap-south-1",
-            Key: "server/assets/users.json",
-        });
-        // const usersData = utils.readUsers();
-        // usersData.push(newUser);
-        // utils.writeUsers(usersData);
+        const usersData = utils.readUsers();
+        usersData.push(newUser);
+        utils.writeUsers(usersData);
         console.log('Account created');
         return res.status(200).send("Account created!");
 });
@@ -52,17 +45,8 @@ router.post("/login", (req, res) => {
     if (!username || !password) {
         res.status(400).send("Please enter in all fields!");
     }
-        // const usersData = utils.readUsers();
-        let my_file = s3.getObject({
-            Bucket: "cyclic-byzantium-cockatoo-hose-ap-south-1",
-            Key: "server/assets/users.json",
-        });
-        // let usersData = [];
-        // usersData.push(my_file.data)
-        console.log(my_file);
-        // console.log(typeof(usersData));
+        const usersData = utils.readUsers();
         let foundUser = usersData.find((user) => user.username === username);
-        // console.log(usersData);
 
         if(foundUser === undefined) {
             return res.status(400).send("Username invalid!");
@@ -78,8 +62,6 @@ router.post("/login", (req, res) => {
             JWT_KEY,
             {expiresIn: "1h"}
         );
-        // Will have to function to check if bcrypt password matches entered password
-        // But first have to create sign up so testing user will begin with crypt password
         res.json({ token });
 });
 
